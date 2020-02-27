2ee754de42778478d465b79f10784db268809fca
kentoje <k.monthubert@gmail.com>
2020-02-27 10:36:37 +0100

    add return type and change expose ports

--- src/Service/JsonMessage.php
+++ src/Service/JsonMessage.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class JsonMessage
 {
-    public function getEmptyDataMessage()
+    public function getEmptyDataMessage(): JsonResponse
     {
         return new JsonResponse(['message' => 'The response does not contain any data.'], Response::HTTP_NOT_FOUND);
     }
